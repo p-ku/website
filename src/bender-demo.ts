d0cd26229ba884cfe0d224a275c3f68d52ae7f8c
Rick <contact@p-ku.com>
2021-02-03 15:52:16 -0600

    Add working demo plot

--- src/bender-demo.ts
+++ src/bender-demo.ts
@@ -9,7 +9,26 @@ class BenderDemo extends LitElement {
   @property({ type: String }) lang = '';
   @property({ type: Boolean }) english = true;
   @property({ attribute: false }) angle = 0;
+  @property({ attribute: false }) section = new THREE.Shape();
+
+  @property({ attribute: false }) tensMat = new THREE.MeshPhongMaterial({
+    color: new THREE.Color(0.5, 0.2, 0.5),
+    polygonOffset: true,
+    polygonOffsetFactor: -1, // positive value pushes polygon further away
+    polygonOffsetUnits: 1,
+    side: THREE.DoubleSide,
+  });
+  @property({ attribute: false }) compMat = new THREE.MeshPhongMaterial({
+    color: new THREE.Color(0.2, 0.5, 0.5),
+    polygonOffset: true,
+    polygonOffsetFactor: -1, // positive value pushes polygon further away
+    polygonOffsetUnits: 1,
+    side: THREE.DoubleSide,
+  });
+  /* @property({ attribute: false }) compMat = new THREE.MeshBasicMaterial({color: new THREE.Color(0.4,1,1), side: THREE.DoubleSide});
+  @property({ attribute: false }) tensMat = new THREE.MeshBasicMaterial({color: new THREE.Color(1,0.4,1), side: THREE.DoubleSide}); */
   @property({ attribute: false }) scene = new THREE.Scene();
+
   @property({ attribute: false }) scene2 = new THREE.Scene();
 
   @property({ attribute: false }) camera = new THREE.PerspectiveCamera(
@@ -65,6 +84,14 @@ class BenderDemo extends LitElement {
       transparent: true,
       side: THREE.DoubleSide,
     });
+    const sectionMat = new THREE.MeshBasicMaterial({
+      color: new THREE.Color(0.2, 0.2, 0.2),
+      polygonOffset: true,
+      polygonOffsetFactor: -1, // positive value pushes polygon further away
+      polygonOffsetUnits: 1,
+      side: THREE.DoubleSide,
+    });
+
     const plane = new THREE.Mesh(geo, mat);
     const planeEdge = new THREE.EdgesGeometry(geo);
     const planemesh = new THREE.LineSegments(
@@ -74,10 +101,32 @@ class BenderDemo extends LitElement {
     plane.rotateY(-Math.PI / 2);
     planeEdge.rotateY(-Math.PI / 2);
 
-    this.scene2.add(plane);
-    this.scene2.add(planemesh);
+    this.section.moveTo(-0.5, -0.5);
+    this.section.lineTo(0.5, -0.5);
+    this.section.lineTo(0.5, -0.4);
+    this.section.lineTo(0.05, -0.4);
+    this.section.lineTo(0.05, 0.4);
+    this.section.lineTo(0.5, 0.4);
+    this.section.lineTo(0.5, 0.5);
+    this.section.lineTo(-0.5, 0.5);
+    this.section.lineTo(-0.5, 0.4);
+    this.section.lineTo(-0.05, 0.4);
+    this.section.lineTo(-0.05, -0.4);
+    this.section.lineTo(-0.5, -0.4);
+    this.section.lineTo(-0.5, -0.5);
+    const sectionGeo = new THREE.ShapeGeometry(this.section);
+    const sectionEdge = new THREE.EdgesGeometry(sectionGeo);
+    const sectionLine = new THREE.LineSegments(
+      sectionEdge,
+      new THREE.LineBasicMaterial({ color: 0x000000 })
+    );
+    sectionLine.rotateY(-Math.PI / 2);
+    sectionGeo.rotateY(-Math.PI / 2);
+    const sectionMesh = new THREE.Mesh(sectionGeo, sectionMat);
+
+    this.scene2.add(plane, sectionLine, planemesh, sectionMesh);
     this.init();
-    this.camera.position.set(0, 3, 4.5); // Set position like this
+    this.camera.position.set(2, 2, 4.5); // Set position like this
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     this.animator();
@@ -99,9 +148,6 @@ class BenderDemo extends LitElement {
     this.scene.background = new THREE.Color(0xfffde8);
     this.scene2.background = new THREE.Color(0xfffde8);
 
-    /*    this.controls.target.set(0, 0, 0);
-    this.controls.update(); */
-
     this.camera.aspect = 3 / 2;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(
@@ -122,81 +168,204 @@ class BenderDemo extends LitElement {
     this.renderer.outputEncoding = THREE.sRGBEncoding;
     this.renderer2.outputEncoding = THREE.sRGBEncoding;
 
-    //
+    /*       if (this.angle != 0) {
+const graphTopGeo = new THREE.PlaneGeometry(1,this.angle/60,1);
+const graphSideFlangeShape = new THREE.Shape();
+graphSideFlangeShape.moveTo(0.5, 0);
+graphSideFlangeShape.lineTo(0.5, this.angle/60);
+graphSideFlangeShape.lineTo(0.4, (this.angle/60)*(0.4)/(0.5));
+graphSideFlangeShape.lineTo(0.5, 0);
 
-    const h = 1,
-      b = 1,
-      tf = 0.1,
-      tw = 0.1;
+
+const graphSideFlangeGeo = new THREE.ShapeGeometry(graphSideFlangeShape);
+const graphSideWebGeo = new THREE.PlaneGeometry(1,this.angle/60,1);
+const graphSlopeGeo = new THREE.PlaneGeometry(1,this.angle/60,1);
+
+
+if (this.angle > 0) {
+      const graphTopMat = new THREE.MeshBasicMaterial({ color: new THREE.Color("rgb(255, 100, 255)"),side: THREE.DoubleSide});
+    const graphTopMesh = new THREE.Mesh(graphTopGeo, graphTopMat);
+    const graphTopEdge = new THREE.EdgesGeometry(graphTopGeo);
+    const planeMesh3 = new THREE.LineSegments(
+      graphTopEdge,
+      new THREE.LineBasicMaterial({ color: 0x000000 })
+    );
+      graphTopEdge.rotateY( - Math.PI / 2);
+      graphTopEdge.rotateZ( - Math.PI / 2);
+      graphTopEdge.translate(this.angle/120, 0.5, 0);
+      graphTopMesh.rotateY( - Math.PI / 2);
+      graphTopMesh.rotateX( - Math.PI / 2);
+      graphTopMesh.translateY(this.angle/120);
+      graphTopMesh.translateZ(0.5);
+
+    this.scene2.add(graphTopMesh);
+    this.scene2.add(planeMesh3);} 
+    else {
+  const graphTopMat = new THREE.MeshBasicMaterial({ color: new THREE.Color("rgb(100, 255, 255)"),side: THREE.DoubleSide});
+  const graphTopMesh = new THREE.Mesh(graphTopGeo, graphTopMat);
+  const graphTopEdge = new THREE.EdgesGeometry(graphTopGeo);
+  const planeMesh3 = new THREE.LineSegments(
+    graphTopEdge,
+    new THREE.LineBasicMaterial({ color: 0x000000 })
+  );
+    graphTopEdge.rotateY( - Math.PI / 2);
+    graphTopEdge.rotateZ( - Math.PI / 2);
+    graphTopEdge.translate(this.angle/120, 0.5, 0);
+    graphTopMesh.rotateY( - Math.PI / 2);
+    graphTopMesh.rotateX( - Math.PI / 2);
+    graphTopMesh.translateY(this.angle/120);
+    graphTopMesh.translateZ(0.5);
+
+  this.scene2.add(graphTopMesh,planeMesh3);
+}
+
+      } */
+
     if (this.angle != 0) {
-      const graphTopGeo = new THREE.PlaneGeometry(1, this.angle / 60, 1);
+      const top = this.angle / 60;
+      const inTop = top - (0.1 / 0.5) * top;
+      const centAng = Math.atan(2 * top);
+      const hyp = Math.sqrt(Math.pow(Math.abs(top), 2) + 0.25);
+      const hyp2 = Math.sqrt(Math.pow(Math.abs(inTop), 2) + 0.16);
 
-      if (this.angle > 0) {
-        const mat2 = new THREE.MeshBasicMaterial({
-          color: new THREE.Color('rgb(255, 100, 255)'),
-          side: THREE.DoubleSide,
-        });
-        const plane3 = new THREE.Mesh(graphTopGeo, mat2);
-        const planeEdge3 = new THREE.EdgesGeometry(graphTopGeo);
-        const planeMesh3 = new THREE.LineSegments(
-          planeEdge3,
-          new THREE.LineBasicMaterial({ color: 0x000000 })
-        );
-        planeEdge3.rotateY(-Math.PI / 2);
-        planeEdge3.rotateZ(-Math.PI / 2);
-        planeEdge3.translate(this.angle / 120, h / 2, 0);
-        plane3.rotateY(-Math.PI / 2);
-        plane3.rotateX(-Math.PI / 2);
-        plane3.translateY(this.angle / 120);
-        plane3.translateZ(h / 2);
-
-        this.scene2.add(plane3);
-        this.scene2.add(planeMesh3);
-      } else {
-        const mat2 = new THREE.MeshBasicMaterial({
-          color: new THREE.Color('rgb(100, 255, 255)'),
-          side: THREE.DoubleSide,
-        });
-        const plane3 = new THREE.Mesh(graphTopGeo, mat2);
-        const planeEdge3 = new THREE.EdgesGeometry(graphTopGeo);
-        const planeMesh3 = new THREE.LineSegments(
-          planeEdge3,
-          new THREE.LineBasicMaterial({ color: 0x000000 })
-        );
-        planeEdge3.rotateY(-Math.PI / 2);
-        planeEdge3.rotateZ(-Math.PI / 2);
-        planeEdge3.translate(this.angle / 120, h / 2, 0);
-        plane3.rotateY(-Math.PI / 2);
-        plane3.rotateX(-Math.PI / 2);
-        plane3.translateY(this.angle / 120);
-        plane3.translateZ(h / 2);
-
-        this.scene2.add(plane3, planeMesh3);
-      }
+      this.graphPlanes(top / 2, 0.5);
+      this.graphShapes(
+        0,
+        0,
+        0.5,
+        Math.PI,
+        0,
+        0,
+        top,
+        -0.5,
+        0,
+        -0.5,
+        0,
+        -0.4,
+        inTop,
+        -0.4,
+        top,
+        -0.5
+      );
+      this.graphShapes(
+        0,
+        0,
+        -0.5,
+        Math.PI,
+        0,
+        0,
+        top,
+        -0.5,
+        0,
+        -0.5,
+        0,
+        -0.4,
+        inTop,
+        -0.4,
+        top,
+        -0.5
+      );
+      this.graphShapes(
+        0,
+        0.4,
+        0,
+        -Math.PI / 2,
+        -Math.PI,
+        Math.PI,
+        inTop,
+        0.05,
+        inTop,
+        0.5,
+        0,
+        0.5,
+        0,
+        0.05,
+        inTop,
+        0.05
+      );
+      this.graphShapes(
+        0,
+        0.4,
+        0,
+        Math.PI / 2,
+        Math.PI,
+        Math.PI,
+        inTop,
+        0.05,
+        inTop,
+        0.5,
+        0,
+        0.5,
+        0,
+        0.05,
+        inTop,
+        0.05
+      );
+      this.graphShapes(
+        0,
+        0,
+        0,
+        -Math.PI / 2,
+        Math.PI,
+        Math.PI / 2 - centAng,
+        -hyp,
+        0.5,
+        -hyp2,
+        0.5,
+        -hyp2,
+        0.05,
+        0,
+        0.05,
+        0,
+        -0.05,
+        -hyp2,
+        -0.05,
+        -hyp2,
+        -0.5,
+        -hyp,
+        -0.5,
+        -hyp,
+        0.5
+      );
+      this.graphShapes(
+        0,
+        0,
+        0.05,
+        Math.PI,
+        0,
+        0,
+        0,
+        -0.4,
+        0,
+        0,
+        inTop,
+        -0.4,
+        inTop,
+        0
+      );
+      this.graphShapes(
+        0,
+        0,
+        -0.05,
+        Math.PI,
+        0,
+        0,
+        0,
+        -0.4,
+        0,
+        0,
+        inTop,
+        -0.4,
+        inTop,
+        0
+      );
+      /*         this.graphShapes(0.2,0,0,0,Math.PI/2,0,-0.05,0,-0.05,0.4,-0.5,0.4,-0.5,0.5,0.5,0.5,0.5,0.4,0.05,0.4,0.05,0,-0.05,0);
+       */
     }
 
-    const shape = new THREE.Shape();
-    shape.moveTo(-b / 2, -h / 2);
-    shape.lineTo(b / 2, -h / 2);
-    shape.lineTo(b / 2, tf - h / 2);
-    shape.lineTo(tw / 2, tf - h / 2);
-    shape.lineTo(tw / 2, h / 2 - tf);
-    shape.lineTo(b / 2, h / 2 - tf);
-    shape.lineTo(b / 2, h / 2);
-    shape.lineTo(-b / 2, h / 2);
-    shape.lineTo(-b / 2, h / 2 - tf);
-    shape.lineTo(-tw / 2, h / 2 - tf);
-    shape.lineTo(-tw / 2, tf - h / 2);
-    shape.lineTo(-b / 2, tf - h / 2);
-    shape.lineTo(-b / 2, -h / 2);
-    const midcurve = new THREE.Vector3(
-      0,
-      -1.5 * Math.tan((this.angle * Math.PI) / 360),
-      0
-    );
     const curve = new THREE.QuadraticBezierCurve3(
       new THREE.Vector3(-1.5, 0, 0),
-      midcurve,
+      new THREE.Vector3(0, -1.5 * Math.tan((this.angle * Math.PI) / 360), 0),
       new THREE.Vector3(1.5, 0, 0)
     );
     const uvtest = THREE.ExtrudeGeometry.WorldUVGenerator;
@@ -208,7 +377,7 @@ class BenderDemo extends LitElement {
       UVGenerator: uvtest,
     };
 
-    const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+    const geometry = new THREE.ExtrudeGeometry(this.section, extrudeSettings);
 
     for (let i = 0; i < geometry.faces.length; i++) {
       const face = geometry.faces[i];
@@ -327,19 +496,19 @@ class BenderDemo extends LitElement {
     const intensity = 0.5;
     const amintensity = 1;
 
-    const light = new THREE.DirectionalLight(color, intensity);
-    const light2 = new THREE.DirectionalLight(color, intensity);
-    const amlight = new THREE.AmbientLight(color, amintensity);
-    const hemlight = new THREE.HemisphereLight(0xffffff, color, intensity);
+    const light1 = new THREE.DirectionalLight(color, intensity);
+    const amlight1 = new THREE.AmbientLight(color, amintensity);
+    const hemlight1 = new THREE.HemisphereLight(0xffffff, color, intensity);
 
-    light.position.set(0, 0, 5);
-    light.target.position.set(0, 0, 0);
-    light2.position.set(0, 0, -5);
-    light2.target.position.set(0, 0, 0);
+    light1.position.set(0, 0, 5);
+    light1.target.position.set(0, 0, 0);
 
-    this.scene.add(light, light2, amlight, hemlight);
-    this.scene.add(light.target);
-    this.scene.add(light2.target);
+    const light2 = light1.clone();
+    const amlight2 = amlight1.clone();
+    const hemlight2 = hemlight1.clone();
+
+    this.scene.add(light1, amlight1, hemlight1);
+    this.scene2.add(light2, amlight2, hemlight2);
 
     const edges = new THREE.EdgesGeometry(geometry);
     const mesh2 = new THREE.LineSegments(
@@ -364,13 +533,98 @@ class BenderDemo extends LitElement {
       const obj = this.scene.children[i];
       this.scene.remove(obj);
     }
-    for (let i = this.scene2.children.length; i > 1; i--) {
+    for (let i = this.scene2.children.length; i > 3; i--) {
       const obj2 = this.scene2.children[i];
       this.scene2.remove(obj2);
     }
     this.angle = Number((this.sliderValue as HTMLInputElement).value);
     this.init();
     this.animator();
+  }
+
+  graphPlanes(trans1: number, trans2: number) {
+    const planeGeo1 = new THREE.PlaneGeometry(1, this.angle / 60, 1, 1);
+
+    const planeEdge1 = new THREE.EdgesGeometry(planeGeo1);
+    const planeLine1 = new THREE.LineSegments(
+      planeEdge1,
+      new THREE.LineBasicMaterial({ color: 0x000000 })
+    );
+
+    const planeMesh1 = new THREE.Mesh(planeGeo1);
+
+    planeEdge1.rotateY(Math.PI / 2);
+    planeEdge1.rotateZ(Math.PI / 2);
+    planeEdge1.translate(trans1, trans2, 0);
+    planeGeo1.rotateY(Math.PI / 2);
+    planeGeo1.rotateZ(Math.PI / 2);
+    planeGeo1.translate(trans1, trans2, 0);
+    const planeGeo2 = planeGeo1.clone().rotateZ(Math.PI);
+    const planeLine2 = planeLine1.clone().rotateZ(Math.PI);
+    const planeMesh2 = new THREE.Mesh(planeGeo2);
+    if (this.angle > 0 && trans1 > 0) {
+      planeMesh1.material = this.tensMat;
+      planeMesh2.material = this.compMat;
+    } else {
+      planeMesh1.material = this.compMat;
+      planeMesh2.material = this.tensMat;
+    }
+    this.scene2.add(planeMesh1, planeMesh2, planeLine1, planeLine2);
+  }
+  graphShapes(
+    trans1: number,
+    trans2: number,
+    trans3: number,
+    rot1: number,
+    rot2: number,
+    rot3: number,
+    ...args: number[]
+  ) {
+    const shape = new THREE.Shape();
+
+    shape.moveTo(args[0], args[1]);
+    for (let i = 2; i < args.length - 2; i += 2) {
+      shape.lineTo(args[i], args[i + 1]);
+    }
+    const shapeGeo1 = new THREE.ShapeGeometry(shape);
+    const shapeEdge1 = new THREE.EdgesGeometry(shapeGeo1);
+
+    const shapeLine1 = new THREE.LineSegments(
+      shapeEdge1,
+      new THREE.LineBasicMaterial({ color: 0x000000 })
+    );
+    const shapeMesh1 = new THREE.Mesh(shapeGeo1);
+
+    shapeEdge1.rotateX(rot1);
+    shapeEdge1.rotateY(rot2);
+    shapeEdge1.rotateZ(rot3);
+
+    shapeGeo1.rotateX(rot1);
+    shapeGeo1.rotateY(rot2);
+    shapeGeo1.rotateZ(rot3);
+
+    shapeEdge1.translate(trans1, trans2, trans3);
+    shapeGeo1.translate(trans1, trans2, trans3);
+    const shapeGeo2 = shapeGeo1.clone().rotateZ(Math.PI);
+    const shapeLine2 = shapeLine1.clone().rotateZ(Math.PI);
+
+    const shapeMesh2 = new THREE.Mesh(shapeGeo2);
+
+    /*   if ((this.angle > 0 && -args[0]>0) || (this.angle < 0 && -args[0]>0)) {
+    shapeMesh1.material = this.tensMat; shapeMesh2.material = this.compMat;
+ } else {
+  shapeMesh1.material = this.compMat; shapeMesh2.material = this.tensMat;
+
+  } */
+
+    if (this.angle > 0) {
+      shapeMesh1.material = this.tensMat;
+      shapeMesh2.material = this.compMat;
+    } else {
+      shapeMesh1.material = this.compMat;
+      shapeMesh2.material = this.tensMat;
+    }
+    this.scene2.add(shapeMesh1, shapeMesh2, shapeLine1, shapeLine2);
   }
 
   static styles = css`
