1cb64d4ae70f456869ede8673ce051f2ca905b6f
p-ku <contact@p-ku.com>
2022-04-27 11:25:34 -0500

    Clean up rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,6 @@
-// import nodeRes from '@rollup/plugin-node-resolve';
-// import { threeMinifier } from '@yushijinhun/three-minifier-rollup';
-// import bab from '@rollup/plugin-babel';
-// import html from '@web/rollup-plugin-html';
-// import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-// import { terser } from 'rollup-plugin-terser';
-// import { generateSW } from 'rollup-plugin-workbox';
-// import path from 'path';
 import copy from 'rollup-plugin-copy';
 import merge from 'deepmerge';
 import { createSpaConfig } from '@open-wc/building-rollup';
-
-// const merge = require('deepmerge');
-// const typescript = require('@rollup/plugin-typescript');
-
-// const { createSpaConfig } = require('../../index.js');
-
-// const { createSpaConfig } = require('./src/createSpaConfig');
 
 const baseConfig = createSpaConfig({
   injectServiceWorker: true,
@@ -27,11 +12,6 @@ const baseConfig = createSpaConfig({
   workbox: true,
 });
 
-// module.exports = merge(baseConfig, {
-//   input: 'index.html',
-//   plugins: [typescript({ experimentalDecorators: true, target: 'es2019' })],
-// });
-
 export default merge(baseConfig, {
   input: 'index.html',
 
@@ -40,8 +20,6 @@ export default merge(baseConfig, {
   plugins: [
     copy({
       targets: [{ src: 'assets/*', dest: './dist' }],
-      // set flatten to false to preserve folder structure
-      //  flatten: false,
     }),
   ],
 });
