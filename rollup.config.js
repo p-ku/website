53eb588ee4585ec108db5cd3264c29f017b080d7
p-ku <contact@p-ku.com>
2026-07-26 15:16:52 -0600

    Catch up on updates

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import html from '@web/rollup-plugin-html';
 import { copy } from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
+// import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 export default {
   plugins: [
@@ -12,8 +12,6 @@ export default {
     }),
     /** Resolve bare module imports */
     resolve(),
-    // Minify HTML template literals
-    minifyHTML(),
     /** Minify JS */
     terser({
       ecma: 2020,
